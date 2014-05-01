47d0ec9e2a3f66b21b2f1d49c8b2cbbe1bd84cfd
Frank Jiang <frankjiang0576@vip.qq.com>
2014-05-01 23:17:48 +0800

    Remove class PathUtils and add the orginal methods to SystemUtils.

--- System Utilities/src/com/frank/sys/SystemUtils.java
+++ System Utilities/src/com/frank/sys/SystemUtils.java
@@ -345,7 +345,7 @@ public class SystemUtils
 	 */
 	public static void writeObject(File file, Object obj) throws IOException
 	{
-		PathUtils.createFileIfNotExist(file);
+		createFileIfNotExist(file);
 		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(
 				file));
 		oos.writeObject(obj);
@@ -417,5 +417,71 @@ public class SystemUtils
 			return false;
 		}
 		return true;
+	}
+	
+	/**
+	 * Returns the file separator of the current system.
+	 * 
+	 * @return the file separator
+	 */
+	public static String getFileSeparator()
+	{
+		return System.getProperty("file.separator");//$NON-NLS-1$
+	}
+
+	/**
+	 * Returns the directory of current project in the format of
+	 * "Disk:/Directory/".
+	 * 
+	 * @return the directory of current project
+	 */
+	public static String getCurrentDirectory()
+	{
+		return System.getProperty("user.dir")//$NON-NLS-1$
+				+ System.getProperty("file.separator");//$NON-NLS-1$
+	}
+
+	/**
+	 * Create the specified file if it not exists.
+	 * 
+	 * @param file
+	 *            the specified file
+	 * @return true if the file exists or created successfully, false otherwise.
+	 * @throws IOException
+	 *             If an I/O error occurred
+	 */
+	public static boolean createFileIfNotExist(File file) throws IOException
+	{
+		if (file.exists())
+			return true;
+		else
+		{
+			File parent = file.getParentFile();
+			if (parent != null && parent.mkdirs())
+				return file.createNewFile();
+			else
+				return false;
+		}
+	}
+
+	/**
+	 * Translate the specified string to UNICODE presented code string.
+	 * 
+	 * @param s
+	 *            the specified string
+	 * @return UNICODE presented code string
+	 */
+	public static String toUnicodeSymbols(String s)
+	{
+		StringBuffer sb = new StringBuffer();
+		char a[] = s.toCharArray();
+		for (char c : a)
+			if (c < 256)
+				sb.append(c);
+			else if (c < 4096)
+				sb.append(String.format("\\u0%h", (int) c));//$NON-NLS-1$
+			else
+				sb.append(String.format("\\u%h", (int) c));//$NON-NLS-1$
+		return sb.toString();
 	}
 }
